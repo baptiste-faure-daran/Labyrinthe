43fba1be0bfaef62770f21f03fce6392b257e94f
Baptiste Faure Daran <baptiste.faure-daran@le-campus-numerique.fr>
2020-11-17 10:48:36 +0100

    affichage 2D des labyrinthes

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 let labys;
+let classes = ['top', 'right', 'bot', 'left'];
 
 $(document).ready(function () {
-    $.getJSON("labyrinthes.json", function(json) {
+    $.getJSON("labyrinthes.json", function (json) {
         labys = json;
-        for (const[key, value] of Object.entries(labys)) {
+        for (const [key, value] of Object.entries(labys)) {
             let elem = `<option value="${key}">${key}</option>`
             $('#lab_id').append(elem);
+            console.log(labys)
         }
     });
-
 });
 
 function readLab() {
-    console.log($(`#lab_id`).val())
-    let laby = labys[$(`#lab_id`).val()];
-    let ex = $(`#ex_id`).val();
+    $('.laby_grid').empty();
+    let laby_id = $('#lab_id').val();
+    let ex_id = $('#ex_id').val();
+    let laby = labys[laby_id]["ex-" + (ex_id - 1)];
+
+    let cell_laby = "<div></div>";
+    $(".laby_grid").css('grid-template-columns', `repeat(${laby_id}, 1fr`);
+    laby.forEach(cell => {
+        let actual_cell = $(cell_laby).appendTo('.laby_grid');
+        let walls = cell["walls"];
+        for (let i = 0; i < walls.length; i++) {
+            if (walls[i]) {
+                actual_cell.addClass(classes[i])
+            }
+        }
+    });
 }
